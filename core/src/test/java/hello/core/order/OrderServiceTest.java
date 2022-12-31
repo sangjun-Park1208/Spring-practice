19441608b5b8a99e0344dc1211e96ad6fece284e
sangjun-Park1208 <sangjun1997@naver.com>
2022-12-31 19:40:15 +0900

    객체 지향 원리 적용

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -8,8 +9,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService = new AppConfig().memberService();
+    OrderService orderService = new AppConfig().orderService();
 
     @Test
     void createOrder() {
